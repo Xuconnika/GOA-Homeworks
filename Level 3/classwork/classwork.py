5965f0358aa63f62b3b6154a22a2431ca317cf76
Xuconnika <xuconika.12@gmail.com>
2024-11-02 14:27:54 +0400

    first commit

---
+++ Level 3/classwork/classwork.py
@@ -0,0 +1,13 @@
+
+
+name = "Nika"
+
+surname = "Xucishvii"
+
+age = "14"
+
+hobby = "programming, basketball."
+
+FavoriteAnimal = "Sabertooth tiger"
+
+print(hobby,FavoriteAnimal,age,name,surname)
